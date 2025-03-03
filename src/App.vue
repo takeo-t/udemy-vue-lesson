3bf8415dec184b71c5510a88e15b81232793b4bc
takeo-t <takeo-t@118satellite.com>
2025-03-03 17:45:51 +0900

    v-on 関数使用の場合

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,14 @@ import { ref } from 'vue'
 // const vueURL = ref('https://jp.vuejs.org/')
 // const vueId = ref('vue-link')
 const count = ref(0)
+function countUP() {
+  count.value++
+}
 </script>
 <template>
   <!-- <a :id="vueId" :href="vueURL">Vue.js</a>
   <p>このリンクのIDは 「{{ vueId }}」です。</p> -->
   <p>count is: {{ count }}</p>
   <button @click="count++">button</button>
+  <button @click="countUP">button</button>
 </template>
