3d3b48e0af9e9be1ebe7072e3a736dea93f40f2c
takeo-t <takeo-t@118satellite.com>
2025-03-04 18:00:58 +0900

    v-on 29

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,14 @@ import { ref } from 'vue'
 // const vueURL = ref('https://jp.vuejs.org/')
 // const vueId = ref('vue-link')
 const count = ref(0)
-function countUP() {
-  count.value++
+function countUP(event, times) {
+  count.value = event.clientX * times
 }
 </script>
 <template>
   <!-- <a :id="vueId" :href="vueURL">Vue.js</a>
   <p>このリンクのIDは 「{{ vueId }}」です。</p> -->
   <p>count is: {{ count }}</p>
-  <button @click="count++">button</button>
-  <button @click="countUP">button</button>
+  <button @click="count = $event.clientX">button</button>
+  <button @click="countUP($event, 5)">button</button>
 </template>
