65c9af1856bfade9b729df488f5d1b5037a220ae
takeo-t <takeo-t@118satellite.com>
2025-03-10 18:28:13 +0900

    38

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,25 @@
 <script setup>
-import { ref, watchEffect } from 'vue'
-const count = ref(0)
-watchEffect(() => {
-  console.log('watchEffect')
-  console.log(count.value)
-  setTimeout(() => {
-    console.log('after 1 second')
-  }, 1000)
-  count.value = 'hello'
+import { ref, watch } from 'vue'
+const count1 = ref(0)
+const count2 = ref(0)
+const count3 = ref(0)
+watch(count1, (newValue, oldValue) => {
+  console.log('watch')
+  console.log('newValue:', newValue)
+  console.log('oldValue:', oldValue)
+  // console.log(count1.value, count2.value, count3.value)
 })
+// watchEffect(() => {
+//   console.log('watchEffect')
+//   console.log(count1.value, count2.value, count3.value)
+// })
+//監視対象を明示的に指定するときは、watchEffectよりもwatchを使う。
 </script>
 <template>
-  <p>{{ count }}</p>
-  <button @click="count++">+1</button>
+  <p>{{ count1 }}(count1)</p>
+  <p>{{ count2 }}(count2)</p>
+  <p>{{ count3 }}(count3)</p>
+  <button @click="count1++">count1++</button>
+  <button @click="count2++">count2++</button>
+  <button @click="count3++">count3++</button>
 </template>
