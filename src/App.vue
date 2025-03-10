f08de7c1688523751abe7a9329ebb4c79b3a9be3
takeo-t <takeo-t@118satellite.com>
2025-03-10 15:30:08 +0900

    33

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <script setup>
 import { ref } from 'vue'
 const count = ref(0)
+const eventName = 'keyup'
 </script>
 <template>
   <p>{{ count }}</p>
-  <input type="text" @keyup.space.delete="count++" />
+  <input type="text" @[eventName].space.delete="count++" />
 </template>
