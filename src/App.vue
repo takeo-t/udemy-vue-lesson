b9ad1eb4b8062322acfc24d1d9af2b8c4c40790c
takeo-t <takeo-t@118satellite.com>
2025-03-10 15:26:08 +0900

    32

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,8 @@
 <script setup>
 import { ref } from 'vue'
-//ディレクティブで変数のデータを表示するときは、トップレベルで変数を宣言する必要がある
-// const vueURL = ref('https://jp.vuejs.org/')
-// const vueId = ref('vue-link')
 const count = ref(0)
-function countUP(event, times) {
-  count.value = event.clientX * times
-}
 </script>
 <template>
-  <!-- <a :id="vueId" :href="vueURL">Vue.js</a>
-  <p>このリンクのIDは 「{{ vueId }}」です。</p> -->
-  <p>count is: {{ count }}</p>
-  <button @click="count = $event.clientX">button</button>
-  <button @click="countUP($event, 5)">button</button>
+  <p>{{ count }}</p>
+  <input type="text" @keyup.space.delete="count++" />
 </template>
