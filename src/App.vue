90a8bdd8a8e7663fa59fc897c3538ff365015430
takeo-t <takeo-t@118satellite.com>
2025-03-10 16:51:25 +0900

    35

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <script setup>
-import { ref } from 'vue'
-const userInput = ref('')
+import { ref, computed } from 'vue'
+const score = ref(0)
+const evaluation = computed(() => {
+  return score.value > 3 ? 'Good' : 'Bad'
+})
+console.log(evaluation.value)
 </script>
 <template>
-  <p>{{ userInput }}</p>
-  <input v-model="userInput" type="text" />
-  <button @click="userInput = 'hi'">button</button>
+  <p>{{ evaluation }}</p>
+  <p>{{ score }}</p>
+  <button @click="score++">+1</button>
 </template>
