ab123cb6b8b5846f9d931f331849e3ea63f3570d
takeo-t <takeo-t@118satellite.com>
2025-03-14 19:39:12 +0900

    56

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <script setup>
 import CountUp from './ CountUp.vue'
+// vue.jsのコンポーネント名はパスカルケース（大文字で始まる）を推奨する
+// コンポーネント名には他コンポーネントとの重複防止のため、2単語以上の使用が推奨される
+// 例：CountUp.vue → CountUp
 </script>
 <template>
   <CountUp />
