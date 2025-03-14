a8d6aded72cfa2d3717a164798883fdd1ea5347a
takeo-t <takeo-t@118satellite.com>
2025-03-14 14:31:46 +0900

    50

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,7 @@
 <script setup>
 import { ref } from 'vue'
-const fruits = ref([
-  { id: 1, name: 'apple' },
-  { id: 2, name: 'banana' },
-  { id: 3, name: 'cherry' },
-  { id: 4, name: 'date' },
-])
-// v-forはtempleteタグを使用することができる
-// v-ifと組み合わせることで、条件に合致した要素のみを描画することができる
+const user = ref({ name: 'John Doe', age: 25, gender: 'man' })
 </script>
 <template>
-  <button @click="fruits.shift()">Remove first</button>
-  <template v-for="{ id, name } in fruits" :key="id">
-    <template v-if="id === 2">
-      <p>id: {{ id }}</p>
-      <input type="text" />{{ name }}({{ index }})
-    </template>
-  </template>
+  <p v-for="(value, key, index) in user" :key="value">{{ key }}: {{ value }}({{ index }})</p>
 </template>
