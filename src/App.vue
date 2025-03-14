87ac390ab0b2c0e67cce33bc990683b41046064d
takeo-t <takeo-t@118satellite.com>
2025-03-14 14:26:10 +0900

    49

--- src/App.vue
+++ src/App.vue
@@ -6,10 +6,15 @@ const fruits = ref([
   { id: 3, name: 'cherry' },
   { id: 4, name: 'date' },
 ])
-// 分割代入でidとnameを取り出すことができる。v-forの中で使うときは{}で囲む
-// indexがなくてもv-forの中で使うことができる
+// v-forはtempleteタグを使用することができる
+// v-ifと組み合わせることで、条件に合致した要素のみを描画することができる
 </script>
 <template>
   <button @click="fruits.shift()">Remove first</button>
-  <li v-for="{ id, name } in fruits" :key="id"><input type="text" />{{ name }}({{ index }})</li>
+  <template v-for="{ id, name } in fruits" :key="id">
+    <template v-if="id === 2">
+      <p>id: {{ id }}</p>
+      <input type="text" />{{ name }}({{ index }})
+    </template>
+  </template>
 </template>
