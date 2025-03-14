4e2cc921d3026defec0c5c42b82fa5e4a3bb58a7
takeo-t <takeo-t@118satellite.com>
2025-03-14 13:38:47 +0900

    43

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script setup>
 import { ref } from 'vue'
 const ok = ref(true)
-const maybeOK = ref(true)
+//  templateタグでv-ifディレクティブを使用すると、レンダリング時にtemplateタグ自体が削除される
 </script>
 <template>
   <button @click="ok = !ok">toggle</button>
-  <p v-if="ok">ok!</p>
-  <p v-else-if="maybeOK">maybe OK!</p>
-  <p v-else>not ok</p>
+  <template v-if="ok">
+    <p>OK!</p>
+    <p>Hello</p>
+  </template>
 </template>
