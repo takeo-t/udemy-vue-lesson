b9b54586e40dff92bfaccedbf8f7d116f418245f
takeo-t <takeo-t@118satellite.com>
2025-03-14 14:18:32 +0900

    48

--- src/App.vue
+++ src/App.vue
@@ -7,10 +7,9 @@ const fruits = ref([
   { id: 4, name: 'date' },
 ])
 // 分割代入でidとnameを取り出すことができる。v-forの中で使うときは{}で囲む
+// indexがなくてもv-forの中で使うことができる
 </script>
 <template>
   <button @click="fruits.shift()">Remove first</button>
-  <li v-for="({ id, name }, index) in fruits" :key="id">
-    <input type="text" />{{ name }}({{ index }})
-  </li>
+  <li v-for="{ id, name } in fruits" :key="id"><input type="text" />{{ name }}({{ index }})</li>
 </template>
