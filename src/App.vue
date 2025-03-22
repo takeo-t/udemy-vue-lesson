1c939066b190413ad572361045946139b559aed2
takeo-t <takeo-t@118satellite.com>
2025-03-22 18:09:46 +0900

    69

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <script setup>
-// 親コンポーネント側でPropsとして属性でデータを渡す
+// 子コンポーネントから親コンポーネントにデータを渡すときはエミットを使用する。
 import { ref } from 'vue'
 import ResetButton from './components/ResetButton.vue'
 
 const count = ref(0)
+function onReset(value) {
+  count.value = value
+}
 </script>
 <template>
   <p>count: {{ count }}</p>
   <button @click="count++">+1</button>
-  <ResetButton @reset="count = 0" />
+  <ResetButton @reset="onReset" />
 </template>
