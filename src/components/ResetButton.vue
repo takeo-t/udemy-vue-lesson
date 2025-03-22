abea338ed8b0bfcc2922332e33bb6873bd2e9544
takeo-t <takeo-t@118satellite.com>
2025-03-22 18:13:03 +0900

    70

--- src/components/ResetButton.vue
+++ src/components/ResetButton.vue
@@ -1,3 +1,10 @@
+<script setup>
+// defineEmits を必ず書く
+const emit = defineEmits(['reset'])
+function emitReset() {
+  emit('reset', 100)
+}
+</script>
 <template>
-  <button @click="$emit('reset', 100)">Reset</button>
+  <button @click="emitReset">Reset</button>
 </template>
