6896fd05a50294830bea2a4b55ed9e2108f0b143
takeo-t <takeo-t@118satellite.com>
2025-03-31 22:21:13 +0900

    81

--- src/components/CountUp.vue
+++ src/components/CountUp.vue
@@ -1,6 +1,34 @@
 <script setup>
-import { ref } from 'vue'
+import {
+  ref,
+  onMounted,
+  onUnmounted,
+  onUpdated,
+  onBeforeMount,
+  onBeforeUpdate,
+  onBeforeUnmount,
+} from 'vue'
 const count = ref(0)
+onBeforeMount(() => {
+  console.log('onBeforeMount')
+})
+onMounted(() => {
+  console.log('onMounted')
+})
+
+onBeforeUpdate(() => {
+  console.log('onBeforeUpdate')
+})
+onUpdated(() => {
+  console.log('onUpdated')
+})
+
+onBeforeUnmount(() => {
+  console.log('onBeforeUnmount')
+})
+onUnmounted(() => {
+  console.log('onUnmounted')
+})
 </script>
 <template>
   <p>count: {{ count }}</p>
